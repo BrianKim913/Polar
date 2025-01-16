d92ee0950f19ba2cd032f72cc6824d6fdae70897
Kim Bum Soo <bumsookim333@naver.com>
2025-01-16 18:14:53 +0900

    add properties for greeting

--- src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
+++ src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
@@ -1,2 +1,20 @@
-package com.polarbookshop.catalogservice.config;public class PolarProperties {
+package com.polarbookshop.catalogservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "polar")
+public class PolarProperties {
+
+    /**
+    * A Message to welcome users.
+    */
+    private String greeting;
+
+    public String getGreeting() {
+        return greeting;
+    }
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
 }
