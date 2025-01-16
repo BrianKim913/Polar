eae16afd9a2b5a522a20e1360e360d6a876d80f2
Kim Bum Soo <bumsookim333@naver.com>
2025-01-17 01:19:01 +0900

    add dataConfig and jdbc audit fields

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,5 +5,7 @@ CREATE TABLE book (
     isbn VARCHAR(255) UNIQUE NOT NULL,
     price float8 NOT NULL,
     title VARCHAR(255) NOT NULL,
+    created_date timestamp NOT NULL,
+    last_modified_data timestamp NOT NULL,
     version integer NOT NULL
 );
