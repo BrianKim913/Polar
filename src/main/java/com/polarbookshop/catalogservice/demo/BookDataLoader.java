57faddce6fd4bb59bd7ef3d76103dc05b61c7a63
Kim Bum Soo <bumsookim333@naver.com>
2025-01-16 18:14:19 +0900

    add testdata for bootrun

---
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,26 @@
+package com.polarbookshop.catalogservice.demo;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testdata") //assign the class to the 'testdata' profile
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    //generate test data when ApplicationReadyEvent is sent(when application start up phase is completed)
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1234567891", "Northern Lights", "Lyra Silvertongue", 9.90 );
+        var book2 = new Book("1234567892", "Polar Journey", "Iorek Byrnison", 12.90);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
